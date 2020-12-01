11aeeb7a191efb1148f4f6d9f86131c04a51014a
Greenfoot5 <greenfoot89@gmail.com>
2020-12-01 16:02:03 +0000

    Completed 01 part 2

--- 01/main.py
+++ 01/main.py
@@ -6,7 +6,15 @@ with open("input.txt", 'r') as f:
     for i in range(len(numbers)):
         numbers[i] = int(numbers[i])
 
+# Part 1
 for num_a in numbers:
     for num_b in numbers:
         if num_a + num_b == 2020:
             print(num_a * num_b)
+
+# Part 2
+for num_a in numbers:
+    for num_b in numbers:
+        for num_c in numbers:
+            if num_a + num_b + num_c == 2020:
+                print(num_a * num_b * num_c)
